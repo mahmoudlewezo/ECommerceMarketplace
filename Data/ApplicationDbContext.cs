a9f10389ef2afd28821d1dd7334262b0cced3065
mahmoud lewezo <mahmoudkhamis7009@gmail.com>
2026-08-18 13:01:44 +0300

    feat: setup identity authentication

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceMarketplace.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
